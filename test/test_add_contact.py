f137098f4969c1acc961a2d338996968765a71a5
anastasiasprygina <nastyasprygina@gmail.com>
2018-02-06 22:21:28 +0300

    Исправления: используется одна и та же фикстура для групп и контактов, файлы упорядочены по соотвествующим пакетам

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contact import Contact
-from application2 import Application2
+from model.contact import Contact
+from fixture.Application import Application
 
 @pytest.fixture
 def app(request):
-    fixture = Application2()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
     
-def test_test_add_contact(app):
-    app.login(name="admin", password="secret")
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="test", middlename="test", lastname="test", nickname="test", title="test", company="test", address="test", home="test", mobile="test", work="test", fax="test", email="test", email2="test", email3="test", homepage="test", byear="2018"))
-    app.logout()
+    app.session.logout()
 
 
 
