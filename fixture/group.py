98f378a27da7a292c6deb3544fe486ffa8609944
anastasiasprygina <nastyasprygina@gmail.com>
2018-02-08 23:26:17 +0300

    delete group, edit group

--- fixture/group.py
+++ fixture/group.py
@@ -36,3 +36,14 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def edit_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+
+
