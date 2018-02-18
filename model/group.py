f5a8de87410a5d0a7ac9afb3d574c7c7e9abe217
anastasiasprygina <nastyasprygina@gmail.com>
2018-02-18 22:27:37 +0300

    10

--- model/group.py
+++ model/group.py
@@ -1,7 +1,8 @@
 class Group:
 
-    def __init__(self, name=None, group_header=None, footer=None):
+    def __init__(self, name=None, group_header=None, footer=None, id=None):
         self.name = name
         self.header = group_header
         self.footer = footer
+        self.id = id
 
