eb55865aaf47fbb2689625de6eded041d6060c9f
anastasiasprygina <nastyasprygina@gmail.com>
2018-03-04 18:15:07 +0300

    15

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "
+    return prefix + " ".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname=" ", middlename=" ", lastname=" ", nickname=" ", title=" ", company=" ", address=" ", home=" ", mobile=" ", work=" ", fax=" ",
+                    email=" ", email2=" ", email3=" ", homepage=" ", byear=" ")] + [Contact(firstname=random_string("testtest", 10), middlename=random_string("testtest", 10), lastname=random_string("testtest", 10), nickname=random_string("testtest", 10), title=random_string("testtest", 10), company=random_string("testtest", 10), address=random_string("testtest", 10), home=random_string("testtest", 10), mobile=random_string("testtest", 10), work=random_string("testtest", 10), fax=random_string("testtest", 10), email=random_string("testtest", 10), email2=random_string("testtest", 10),
+                    email3=random_string("testtest", 10), homepage=random_string("testtest", 10),
+                    byear=random_string("testtest", 10))
+                    for i in range(5)]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="test", middlename="test", lastname="test", nickname="test", title="test", company="test", address="test", home="test", mobile="test", work="test", fax="test", email="test", email2="test", email3="test", homepage="test", byear="2018")
     app.contact.create_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
