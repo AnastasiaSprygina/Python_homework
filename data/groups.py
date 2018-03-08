d3c8ea5768a6eb971583f8014cf18b4043033ee6
anastasiasprygina <nastyasprygina@gmail.com>
2018-03-08 16:03:10 +0300

    18.2

---
+++ data/groups.py
@@ -0,0 +1,11 @@
+from model.group import Group
+
+
+testdata = [
+    Group(name="name1", group_header="header1", footer="footer1"),
+Group(name="name2", group_header="header2", footer="footer2")
+]
+
+
+
+
